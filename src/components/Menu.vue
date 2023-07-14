f8fb820eb2f27e755988dfbafe3e373126eea39e
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-07-14 09:46:07 -0300

    fix: updated CSS styles and all styles are inside styles.css
    deleted default main.css and base.css from vue template

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,8 +2,10 @@
 
 <template>
 <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/store">Store</RouterLink>
-  </nav>
+  <div class="nav-buttons">
+     <div class="home-select"><RouterLink to="/">Home</RouterLink></div>
+    <div class="about-select"><RouterLink to="/about">About</RouterLink></div>
+    <div class="store-select"><RouterLink to="/store">Store</RouterLink></div>
+  </div></nav>
 </template>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,4 +3,5 @@
 
 <template>
 <h1>Home</h1>
+<span class="home-view">This is a home view</span>
 </template>
